5540947fe2a2e3fd1b72e7ee59b6121ee13eeb72
tcly861204 <tcly861204@hotmail.com>
2022-01-07 12:02:40 +0800

    feat: 文件添加作者信息

--- src/bigData/header.jsx
+++ src/bigData/header.jsx
@@ -1,3 +1,11 @@
+/*
+  @Author: tcly861204
+  @Email:  356671808@qq.com
+  @Date:   2022/1/7 下午12:00:45
+  @Last Modified by:   tcly861204
+  @Last Modified time: 2022/1/7 下午12:00:45
+  @Github: https://tcly861204.github.io
+*/
 import React, { useState, useRef, useEffect, useCallback } from 'react'
 import PropTypes from 'prop-types'
 import Checkbox from './checkbox'
